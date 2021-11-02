1bb29f8e0845ff1347979f44775cbf23a1136393
Priyanshu Bhardwaj <63417899+priyanshu-beep@users.noreply.github.com>
2021-11-03 02:07:33 +0530

    Update user.js

--- mvc/controllers/user.js
+++ mvc/controllers/user.js
@@ -12,7 +12,6 @@ const auth = function (req, res) {
   const CLIENT_ID = '966641561326-7nivlb5sna5dpcloaot4d51oba9n46ej.apps.googleusercontent.com'
   const client = new OAuth2Client(CLIENT_ID);
   let token = req.body.token;
-  // console.log(token)
   let profile = req.body.profile;
   let user = new User();
   user.name = profile.Se;
@@ -38,8 +37,6 @@ const auth = function (req, res) {
   }
   verify()
     .then(() => {
-      //       let tmp = JSON.parse(token);
-      // console.log(tmp);
       res.cookie('session-token', token);
       res.send('success');
     })
@@ -50,57 +47,68 @@ const auth = function (req, res) {
 
 
 const clist = async function () {
-  let clist_token = 'username=priyanshu_x&api_key=1e0a1d7b14cf84194ca2a240efc8e735b28b9422'
-  let URL_BASE = 'https://clist.by/api/v2/'
 
-  var d = new Date();
-  d.setDate(d.getDate() - 2);
+  return new Promise(function (resolve, reject) {
 
-  // console.log(d)
-  var c = JSON.stringify(d)
-  c = c.substring(1)
-  c = c.slice(0, -1)
+    let clist_token = 'username=priyanshu_x&api_key=1e0a1d7b14cf84194ca2a240efc8e735b28b9422'
+    let URL_BASE = 'https://clist.by/api/v2/'
 
-  // console.log(c)
+    var d = new Date();
+    d.setDate(d.getDate() - 2);
 
-  let url = URL_BASE + 'contest/?limit=20&start__gte=' + c + '&' + clist_token
-  let contestsAdded = [];
-  // console.log(url)
 
-  axios.get(url)
-    .then(r => {
-      let contests = r.data.objects;
-      for (let contest of contests) {
-        let newContest = new Contest();
-        newContest.host = contest.host;
-        newContest.id = contest.id;
-        newContest.link = contest.href;
-        newContest.event = contest.event;
-        newContest.start = contest.start;
-        newContest.end = contest.end;
-        // console.log(newContest);
-        newContest.save((err, val) => {
-          if (err) {
-            if (err.errmsg && err.errmsg.includes("duplicate key error") && err.errmsg.includes("email")) {
-              console.log("Email already exists");
-            } else console.log("Something went wrong ", err);
-          } else {
-            contestsAdded.push(newContest);
-          }
-        })
-      }
-      console.log( "contest added successfully first");
-    })
-    .catch(error => {
-      console.log(error);
-    })
+    var c = JSON.stringify(d)
+    c = c.substring(1)
+    c = c.slice(0, -1)
+
+
+    let url = URL_BASE + 'contest/?limit=20&start__gte=' + c + '&' + clist_token
+    let contestsAdded = [];
+
+
+    axios.get(url)
+      .then(r => {
+        let contests = r.data.objects;
+        for (let contest of contests) {
+          let newContest = new Contest();
+          newContest.host = contest.host;
+          newContest.id = contest.id;
+          newContest.link = contest.href;
+          newContest.event = contest.event;
+          newContest.start = contest.start;
+          newContest.end = contest.end;
+
+          newContest.save((err, val) => {
+
+            if (err) {
+              if (err.errmsg && err.errmsg.includes("duplicate key error") && err.errmsg.includes("email")) {
+                console.log("Email already exists");
+              } else console.log("Something went wrong ");
+            } else {
+              contestsAdded.push(newContest);
+            }
+          })
+        }
+
+        resolve("Fetched data correctly from the clist added it to the database")
+      })
+      .catch(error => {
+        console.log(error);
+        resolve(error)
+      })
+  });
 }
 
-const addCalendar = async function () {
-  // clist()
-  //   .then(() => {
 
-      console.log("ab yaha")
+
+const addCalendar = async function () {
+
+  clist()
+    .then(returnValefromClist => {
+      console.log(returnValefromClist)
+
+
+
       // Create a new instance of oAuth and set our Client ID & Client Secret.
       const oAuth2Client = new OAuth2(
         '966641561326-7nivlb5sna5dpcloaot4d51oba9n46ej.apps.googleusercontent.com',
@@ -117,33 +125,72 @@ const addCalendar = async function () {
       const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
 
 
-      // Create a new event start date instance for temp uses in our calendar.
-      // var eventStartTime = new Date()
-      // eventStartTime.setDate(eventStartTime.getDay())
+      // var allRecentContest
+      const retriveContestData = new Promise((resolve, reject) => {
+        Contest.find((err, contests) => {
+          // console.log(contests)
+          if (err) {
+            console.log("error at 125", err);
+            return res.json({ err: err })
+          }
+          else {
+            resolve(contests);
+          }
+        })
+      })
+      const retriveUserData = new Promise((resolve, reject) => {
+        User.find((err, users) => {
+          if (err) {
+            console.log("error at 145", err);
+            return res.json({ err: err })
+          }
+          else {
+            resolve(users);
+          }
+        })
+      })
 
-      // Create a new event end date instance for temp uses in our calendar.
-      // var eventEndTime = new Date()
-      // eventEndTime.setDate(eventEndTime.getDay())
-      // eventEndTime.setMinutes(eventEndTime.getMinutes() + 45)
+      var newEventCreater = function (event) {
 
-      // Create a dummy event for temp uses in our calendar
+        return new Promise(function (resolve, reject) {
+          calendar.events.insert(
+            { calendarId: 'primary', resource: event },
+            err => {
 
-      // console.log("hdak")
-      Contest.find((err, contests) => {
-        // console.log(contests)
-        if (err) {
-          console.log("error at 125", err);
-        } 
-        else {
-          for (let contest of contests) {
+              if (err) {
+                console.log('Error Creating Calender Event: Event already present')
+                resolve(event)
+              }
+              // Else log that the event was created.
+              else {
+                console.log("-----------------shuru-------------------")
+                console.log('Calendar event successfully created.');
+                console.log(event.id)
+                console.log(event.start.dateTime)
+                console.log(event.start.dateTime)
+                console.log("-----------------khtm-------------------")
 
+                resolve(event)
+              }
+            }
+          )
+        });
+
+      }
+
+
+
+
+      Promise.all([retriveContestData, retriveUserData])
+        .then(async function (values) {
+
+
+          for (let contest of values[0]) {
             // console.log(contest)
-            // break;
-            
-            let tmp = contest.event + contest.link;
             var event = {
               summary: contest.event,
-              description: tmp,
+              description: contest.link,
+              id: 'aaabbbaaacd' + contest.id,
               colorId: 1,
               start: {
                 dateTime: contest.start,
@@ -153,111 +200,152 @@ const addCalendar = async function () {
                 dateTime: contest.end,
                 timeZone: 'IST',
               },
-              attendees: [{
-                email: 'abc'
-              }]
+              attendees: []
+            };
+
+
+
+
+
+            for (let user of values[1]) {
+              event.attendees.push({ email: user.email })
+              if (user.addedContests.find(
+                function (vals) {
+                  if (vals == contest.id)
+                    return true;
+                  else
+                    return false;
+                }
+              ) == undefined) {
+                // save that this contest is added for the user
+                user.addedContests.push(contest.id);
+                user.save();
+              }
+
             }
 
-            // console.log(event)
-            // break;
-            // console.log(event)
-            User.find((err, users) => {
-              if (err) console.log("error at 145", err);
-              else {
-                console.log("aaaaaaaaaaaa")
-                console.log(users)
-                for (let user of users) {
-                  // console.log(event.start.dateTime);
-                  // console.log(user.addedContests)
-                  // console.log("ddddddddddddddddddddddddddddd")
-                  // console.log(filterIt(user.addedContests, contest.id))
 
-                  var flag = 0
-
-                  for (let Id of user.addedContests) {
-                    if (Id == contest.id) {
-                      flag = 1;
-                      break;
+            newEventCreater(event)
+              .then(eventforUpdate => {
+                setTimeout(() => {
+                  var params = {
+                    calendarId: 'primary',
+                    eventId: eventforUpdate.id,
+                    resource: eventforUpdate
+                  };
+  
+  
+                  calendar.events.update(params, function (err) {
+                    if (err) {
+                      console.log('The API returned an error: ' + err);
+                      return;
                     }
-                  }
+                    console.log('Event updated.');
+                  });
+                }, 10000);
+              })
+              .catch(err => {
+                console.log(err)
+              })
 
-                  // console.log(flag)
-                  // console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-                  if (flag == 0) {
-                    event.attendees[0].email = user.email;
-                  // console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+          }
 
-                  //   console.log(event)
-                  // console.log("ddddddddddddddddddddddddddddd")
-
-                    calendar.freebusy.query(
-                      {
-                        resource: {
-                          timeMin: event.start.dateTime,
-                          timeMax: event.end.dateTime,
-                          timeZone: 'IST',
-                          items: [{ id: 'primary' }],
-                        },
-                      },
-                      (err, val) => {
-                        // Check for errors in our query and log them if they exist.
-                        if (err) console.error('Free Busy Query Error: ', err);
-                        else {
-
-                          // Create an array of all events on our calendar during that time.
-                          const eventArr = val.data.calendars.primary.busy
-
-                          // Check if event array is empty which means we are not busy
-
-                          // console.log(eventStartTime, "  and ", eventEndTime);
-                          // If we are not busy create a new calendar event.
-                          calendar.events.insert(
-                            { calendarId: 'primary', resource: event },
-                            err => {
-                              // Check for errors and log them if they exist.
-                              if (err) console.log('Error Creating Calender Event:', err)
-                              // Else log that the event was created.
-                              else {
-                                console.log("-----------------shuru-------------------")
-                                console.log('Calendar event successfully created.');
-                                console.log(contest.start)
-                                console.log(contest.end)
-                                console.log("-----------------khtm-------------------")
-                                user.addedContests.push(contest.id);
-                                user.save();
-                              }
-                            }
-                          )
-                        }
-
-                        // If event array is not empty log that we are busy.
+        })
+        .catch(err => {
+          console.log("Error occured in making promises: ", err)
+        })
 
 
-                      }
-                    )
-                  }
-                  else {
-                    console.log("-----------------shuru-------------------")
-                    console.log('Calendar event was already present.');
-                    console.log(contest.start)
-                    console.log(contest.end)
-                    console.log("-----------------khtm-------------------")
-                  }
-                }
+      console.log("Events updated successfully");
+    })
+    .then(err=> {
+      console.log(err)
+    })
+}
+
+// Useless Stuffs
+const clearCalendar = function (req, res) {
+
+  const oAuth2Client = new OAuth2(
+    '966641561326-7nivlb5sna5dpcloaot4d51oba9n46ej.apps.googleusercontent.com',
+    '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'
+  )
+
+  // Call the setCredentials method on our oAuth2Client instance and set our refresh token.
+  oAuth2Client.setCredentials({
+    refresh_token: '1//04iiZC-nUI3osCgYIARAAGAQSNwF-L9IrJ6GfxfDrVx6wDvoJCzo8Kkl52y63LTtjYsA7srILnQ3YiXY018vmW1Lr7saYyEy8Qww',
+  })
+
+
+  // Create a new calender instance.
+  const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
+
+
+  // var allRecentContest
+  const retriveContestData = new Promise((resolve, reject) => {
+    Contest.find((err, contests) => {
+      // console.log(contests)
+      if (err) {
+        console.log("error at 125", err);
+        return res.json({ err: err })
+      }
+      else {
+        resolve(contests);
+      }
+    })
+  })
+  const retriveUserData = new Promise((resolve, reject) => {
+    User.find((err, users) => {
+      if (err) {
+        console.log("error at 145", err);
+        return res.json({ err: err })
+      }
+      else {
+        resolve(users);
+      }
+    })
+  })
+
+
+
+  Promise.all([retriveContestData, retriveUserData])
+    .then(values => {
+      for (let contest of values[0]) {
+        for (let user of values[1]) {
+
+          if (user.addedContests.find(
+            function (vals) {
+              if (vals == contest.id)
+                return true;
+              else
+                return false;
+            }
+          ) == undefined) {
+            // do nothing if not present
+          }
+          else {
+
+            var params = {
+              calendarId: 'primary',
+              eventId: 'aabba' + contest.id,
+            };
+
+            calendar.events.delete(params, function (err) {
+              if (err) {
+                console.log('The API returned an error: ' + err);
+                return;
               }
-            })
-
-            // break;
+              console.log('Event deleted.');
+            });
           }
         }
 
-      })
-
-      console.log("Events updated successfully");
-
-    // })
+      }
+    })
+    .catch(err => {
+      console.log("Error occured in making promises: ", err)
+    })
 }
-
 const deleteAllUsers = function (req, res) {
   User.deleteMany({}, (err, info) => {
     if (err) {
@@ -280,9 +368,10 @@ const deleteAllContest = function (req, res) {
 
 module.exports = {
   // register,
-  clist,
+  // clist,
   auth,
-  addCalendar,
-  deleteAllUsers,
-  deleteAllContest
+  addCalendar
+  // deleteAllUsers,
+  // deleteAllContest,
+  // clearCalendar
 }
